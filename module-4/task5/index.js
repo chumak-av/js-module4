7da6bc89b0dcc22b48acf8c19de4087f53a3ed8f
chumak-av <57865550+chumak-av@users.noreply.github.com>
2020-04-18 17:09:14 +0300

    2

--- module-4/task5/index.js
+++ module-4/task5/index.js
@@ -1,6 +1,7 @@
 const array = [5, 3, 6, 2, 4];
-for (let i = 0; i < 5; i += 1) {
+let result = 0;
+for (let i = 0; i < array.length; i += 1) {
     console.log(array[i]);
+    result += array[i];
 }
-const result = array[0] + array[1] + array[2] + array[3] + array[4]
 console.log(result);
--- module-4/task7/index.js
+++ module-4/task7/index.js
@@ -1,6 +1,6 @@
 let string = "";
 const array = [1, 2, 3, 4, 5, 6];
-for (let i = 0; i < 6; i += 1) {
+for (let i = 0; i < string.length; i += 1) {
     string += `${array[i]}-`;
 }
 console.log(string);
